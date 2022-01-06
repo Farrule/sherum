a85440f4449cab495e9e44ece8d187b6f2dff0fd
farrule <tsukasap0921@gmail.com>
2022-01-06 12:37:40 +0900

    :tips: BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:sherum/pages/homepage.dart';
+import 'package:sherum/pages/albumpage.dart';
+import 'package:sherum/pages/findpage.dart';
+import 'package:sherum/pages/mypage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'sherum',
+      title: 'sherum main',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'sherum Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
+      home: HomePage(),
+      routes: <String, WidgetBuilder>{
+        '/homepage': (BuildContext context) => HomePage(),
+        '/albumpage': (BuildContext context) => AlbumPage(),
+      },
     );
   }
 }
